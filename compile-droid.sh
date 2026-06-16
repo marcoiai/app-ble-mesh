36a46541914664eab519e2fcad2e5d6d00e4710a
Marco Simao <marco.a.simao@gmail.com>
2026-06-16 18:22:39 -0300

    fix: build Droid APK through Tauri CLI

--- compile-droid.sh
+++ compile-droid.sh
@@ -3,5 +3,8 @@ set -euo pipefail
 
 cd "$(dirname "$0")/src-tauri/gen/android"
 
-./gradlew :app:assembleArm64Debug
-adb install -r app/build/outputs/apk/arm64/debug/app-arm64-debug.apk
+cd ../../..
+
+pnpm tauri android build --debug --target aarch64 --apk --split-per-abi
+adb install -r src-tauri/gen/android/app/build/outputs/apk/arm64/debug/app-arm64-debug.apk
+adb shell monkey -p com.auser.app_ble_mesh -c android.intent.category.LAUNCHER 1
